ec7d7dc792ec787d307c31a52a5ed360892480fe
alimulhuq <alimulhuq3179@gmail.com>
2025-08-22 14:19:25 +0600

    server connection and gracefull shut down added

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,58 @@
-package main;
+package main
 
-import "fmt";
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-func main(){
-	fmt.Println("Welcome to Students API");
+	"github.com/alimulhuq/students-api/internal/config"
+)
+
+func main() {
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Student API"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server Started", slog.String("Address", cfg.HTTPServer.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal("Failed to start Server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting Down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shut Down", slog.String("Error", err.Error()))
+	}
+
+	slog.Info("Server Shut Down Successfuly")
 }
--- internal/config/confing.go
+++ internal/config/confing.go
@@ -1,50 +1,46 @@
-package config;
+package config
 
-import( 
-	"os"
+import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
-);
+)
 
-type HTTPserver struct{
-	addr string
-}
-type Config struct{
-	Env string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
-	Storage_path string `yaml:"storage_path" env-required:"true"`
-	HTTPserver `yaml: "http_server"`
+// HTTPServer config section
+type HTTPServer struct {
+	Addr string `yaml:"address" env:"HTTP_SERVER_ADDR" env-required:"true"`
 }
 
+// Config main structure
+type Config struct {
+	Env         string     `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string     `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http_server"`
+}
 
+// MustLoad loads config or exits
 func MustLoad() *Config {
-	var configPath string;
-	configPath = os.Getenv("CONFIG_PATH");
+	var configPath string
 
-	if(configPath == ""){
-		flags := flag.String("config", "", "Path to the configuration file");
-		flag.Parse();
-		
-		configPath = *flags;
+	if configPath = os.Getenv("CONFIG_PATH"); configPath == "" {
+		flags := flag.String("config", "", "Path to the configuration file")
+		flag.Parse()
 
-		if(configPath == ""){
-			log.Fatal("Config Path is not set");
+		if configPath = *flags; configPath == "" {
+			log.Fatal("Config Path is not set")
 		}
 	}
 
-	if _, err := os.Stat(configPath); os.IsNotExist(err){
-		log.Fatalf("Config file does not exist : %s", configPath);
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
 	}
 
-	var cfg Config;
-
-	err := cleanenv.ReadConfig(configPath, &cfg);
-
-	if(err != nill){
-		log.Fatalf("can not read config file : %s", err.Error());
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
 	}
 
-
-	return &cfg;
+	return &cfg
 }
